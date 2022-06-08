b7bbdf717738a0ec366ab720d5971ccc7341fd5e
MohammadReza Rahmatzadeh <m.rahmatzadeh@emofid.com>
2022-06-08 15:59:14 +0430

    use link and navlink component to avoid referesh

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -13,13 +13,13 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <a className="nav-link" href="/users">Users</a>
+                            <NavLink className="nav-link" to="/users">Users</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/login">Login</a>
+                            <NavLink className="nav-link" to="/login">Login</NavLink>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/register">Register</a>
+                            <NavLink className="nav-link" to="/register">Register</NavLink>
                         </li>
                     </ul>
                 </div>
