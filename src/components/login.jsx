810435ddb4449a92cda9f69b2077ae2c05229e7a
MohammadReza Rahmatzadeh <m.rahmatzadeh@emofid.com>
2022-06-10 22:05:04 +0430

    form validation with yup

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import Input from './input';
+import * as yup from 'yup';
+import axios from 'axios';
 
 class Login extends Component {
     state = {
         account: {
             email: '',
             password: ''
+        },
+        errors: []
+    }
+
+    schema = yup.object.shape(
+        {
+            email: yup.string().email('فرمت ایمیل صحیح نمی باشد').required('ایمیل اجباری است'),
+            password: yup.string().min(4, 'پسورد حداقل بایستی 4 کاراکتر باشد')
+        }
+    );
+
+    validate = async () => {
+        try {
+            const result = await this.schema.validate(this.state.account, { abortEarly: false });
+            return result;
+        } catch (error) {
+            console.log(error.errors);
+            this.setState({ errors: error.errors });
         }
     }
+
     handleSubmit = async (e) => {
         e.preventDefault();
-
-
+        const result = await this.validate();
+        if (result) {
+            const response = await axios.post('https://reqres.in/api/login', result);
+        }
     }
 
     handleChange = async (e) => {
