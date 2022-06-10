39f54c6a3bc30c79794d2ce485095e5b0adcb7e8
MohammadReza Rahmatzadeh <m.rahmatzadeh@emofid.com>
2022-06-10 21:33:22 +0430

    useRef in functional component

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import queryString from 'query-string';
 
@@ -8,6 +8,7 @@ const User = (props) => {
     const location = useLocation();
     const [user, setUser] = useState({});
     const navigate = useNavigate();
+    const firstName = useRef(null);
 
     console.log(queryString.parse(location.search));
 
@@ -15,13 +16,14 @@ const User = (props) => {
         {
             const response = await axios.get(`https://reqres.in/api/users/${id}`);
             setUser(response.data.data);
+            console.log(firstName.current);
         }
     }, []);
 
     return (
         <>
             <img src={user.avatar} style={{ borderRadius: '50%', width: '100px' }} alt="" />
-            <h4>{user.first_name} {user.last_name}</h4>
+            <h4 ref={firstName}>{user.first_name} {user.last_name}</h4>
             <h5>{user.email}</h5>
             <button onClick={() => { navigate('/users'); }} className='btn btn-info mt-3'>Users</button>
         </>);
